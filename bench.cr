f9a7f68854595dc6e6a06a628188966bdb151ca9
Konstantin Makarchev <kostya27@gmail.com>
2016-01-31 17:36:49 +0300

    add bench

---
+++ bench.cr
@@ -0,0 +1,25 @@
+require "./src/memory_cache"
+require "benchmark"
+
+hash = {} of String => String
+cache = MemoryCache(String, String).new
+
+1000.times do |i|
+  hash["key#{i}"] = "value#{i}"
+  cache.write "key#{i}", "value#{i}"
+end
+
+Benchmark.ips do |b|
+  b.report("hash write") { hash["bla"] = "haha" }
+  b.report("cache write") { cache.write "bla", "haha" }
+end
+
+Benchmark.ips do |b|
+  b.report("hash read") { hash["bla"]? }
+  b.report("cache read") { cache.read "bla" }
+end
+
+Benchmark.ips do |b|
+  b.report("hash fetch") { if v = hash["bla"]?; v; else; v = "haha"; hash["bla"] = v; v end }
+  b.report("cache fetch") { cache.fetch("bla") { "haha" } }
+end
