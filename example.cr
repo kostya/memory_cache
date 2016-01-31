7a111e33a56bc6639fec42b7155239b9497f9d3d
Konstantin Makarchev <kostya27@gmail.com>
2016-01-31 17:06:27 +0300

    add example

---
+++ example.cr
@@ -0,0 +1,19 @@
+require "./src/memory_cache"
+
+cache = MemoryCache(String, Int32).new
+
+cache.write("bla", 1)
+p cache.read("bla") # => 1
+
+cache.fetch("haha") { 2 }
+p cache.read("haha") # => 2
+
+cache.write("expired1", 1, expires_in: 1.second)
+p cache.read("expired1") # => 1
+sleep 1
+p cache.read("expired1") # => nil
+
+p cache.fetch("expired1", expires_in: 1.second) { 2 } # => {:fetch, 2}
+p cache.fetch("expired1", expires_in: 1.second) { 2 } # => {:cache, 2}
+sleep 1
+p cache.fetch("expired1", expires_in: 1.second) { 2 } # => {:fetch, 2}
